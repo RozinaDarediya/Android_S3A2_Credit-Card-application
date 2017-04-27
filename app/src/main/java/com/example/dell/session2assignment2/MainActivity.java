09a26ecb8decbc762d5c89284b3ae1c686143cc1
RozinaDarediya <rozy_darediya@ymail.com>
2017-04-27 16:15:27 +0530

    Edited xml file

--- app/src/main/java/com/example/dell/session2assignment2/MainActivity.java
+++ app/src/main/java/com/example/dell/session2assignment2/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       setContentView(R.layout.credit_card_class);
+       setContentView(R.layout.activity_main);
     }
 }
